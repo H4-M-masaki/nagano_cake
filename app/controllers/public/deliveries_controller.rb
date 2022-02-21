b6f7ef3fd68bcb24bfff221c02037c42d5d492eb
EC2 Default User <ec2-user@ip-172-31-46-97.ec2.internal>
2022-02-21 13:16:19 +0000

    [add]

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,7 +1,21 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @customer = Customer.new
+    @customers = Customer.all
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def create
+  end
+  
+  
+  private
+  
+  
+  def customer_params
+    params.require(:customer).permit(:name,:address,:postal_code)
   end
 end
