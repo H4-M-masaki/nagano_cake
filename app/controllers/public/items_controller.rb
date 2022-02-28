d0f8a33443242feb528a1f5e9b8d0a05c295166d
EC2 Default User <ec2-user@ip-172-31-46-97.ec2.internal>
2022-02-28 15:41:56 +0000

    商品関係のページupdate

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,19 @@
 class Public::ItemsController < ApplicationController
   def index
+   @items = Product.all
+   @genres = Genre.all
   end
 
   def show
+   # @item = Item.find(params[:id])
+    @item = Product.find(params[:id])
   end
   
   
+  private
   
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active)
+  end
   
 end
