286082550df7cfc9b864877621150b8fbf29796e
EC2 Default User <ec2-user@ip-172-31-46-97.ec2.internal>
2022-03-05 12:39:49 +0000

    注文関連追加修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,13 @@
 class Public::OrdersController < ApplicationController
   def new
-    
+    @order = Order.new
   end
 
   def check
+    @cart_items = current_customer.cart_items
+    @customer = current_customer
+    @cart_item = CartItem.new
+    @total = 0
   end
 
   def complete
