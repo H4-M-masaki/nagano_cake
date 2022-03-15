844077920f688ba499bbae9373404b1e5a9cb2e9
EC2 Default User <ec2-user@ip-172-31-46-97.ec2.internal>
2022-03-15 10:41:06 +0000

    追加修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,9 +13,8 @@ class Admin::CustomersController < ApplicationController
   
   def update
     @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to admin_customer_path(@customer.id)
-    end
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
   
   
@@ -23,7 +22,7 @@ class Admin::CustomersController < ApplicationController
   private
   
   def customer_params
-   params.require(:customer).permit(:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active)
+   params.require(:customer).permit(:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active, :is_deleted)
   end
   
   
